48df46706222590952cd22fd5762e26aef7fddcd
Grigory-Shade <33571244+Grigory-Shade@users.noreply.github.com>
2018-08-20 16:09:00 +0700

    Task 3

--- 2.php
+++ 2.php
@@ -1,10 +1,10 @@
 <pre>
 <?php
-const all = 80;
-echo 'Всего нарисовано ' .all. ' рисунков,' .PHP_EOL;
-const marker = 23;
-echo 'из них '.marker. ' нарисовано фломастерами,' .PHP_EOL;
-const pencil = 40;
-echo pencil. ' карандашами, а остальные красками.'.PHP_EOL. 'Сколько рисунков выполнено красками?'.PHP_EOL;
-const paint = all - marker - pencil;
-echo 'ОТВЕТ: ' .paint. ' рисунков.';
+const ALL = 80;
+echo 'Всего нарисовано ' .ALL. ' рисунков,' .PHP_EOL;
+const MARKER = 23;
+echo 'из них '.MARKER. ' нарисовано фломастерами,' .PHP_EOL;
+const PENCIL = 40;
+echo PENCIL. ' карандашами, а остальные красками.'.PHP_EOL. 'Сколько рисунков выполнено красками?'.PHP_EOL;
+const PAINT = ALL - MARKER - PENCIL;
+echo 'ОТВЕТ: ' .PAINT. ' рисунков.';
