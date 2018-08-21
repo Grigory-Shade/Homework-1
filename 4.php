8b5c7a32987f18c853ac942da92527c48538577e
Grigory-Shade <33571244+Grigory-Shade@users.noreply.github.com>
2018-08-21 13:35:25 +0700

    Task 4.1

--- 4.php
+++ 4.php
@@ -3,15 +3,10 @@
 $day = rand(0, 10);
 echo 'Порядковый номер дня недели: ' .$day .PHP_EOL;
 switch ($day) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
+    case $day >= 1 && $day <=5:
         echo 'Это рабочий день';
         break;
-    case 6:
-    case 7:
+    case $day == 6 || $day == 7:
         echo 'Это выходной день';
         break;
     default:
